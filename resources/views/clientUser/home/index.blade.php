31cbd12ccb159c34ad14ab4d0bd24ae0a91948f7
Al Zilqad <alzilqad@gmail.com>
2021-01-03 22:42:05 +0600

    home page routing fix

--- resources/views/clientUser/home/index.blade.php
+++ resources/views/clientUser/home/index.blade.php
@@ -8,19 +8,19 @@
           <div class="col-4 manager-dashboard-boxes text-center pt-5">
             <h6 class="mb-3">Affiliated Companies</h6>
             <button class="btn btn-link border-light mb-5">
-              <a class="text-center text-light text-decoration-none" href="/client/company">Manage Companies</a>
+              <a class="text-center text-light text-decoration-none" href="{{route('company.index')}}">Manage Companies</a>
             </button>
           </div>
           <div class="col-4 manager-dashboard-boxes text-center pt-5" style="background: #5f9f9f">
             <h6 class="mb-3">Proposal List</h6>
             <button class="btn btn-link border-light mb-5">
-              <a class="text-center text-light text-decoration-none" href="/proposallist">Manage Proposals</a>
+              <a class="text-center text-light text-decoration-none" href="{{route('proposal.index')}}">Manage Proposals</a>
             </button>
           </div>
           <div class="col-4 manager-dashboard-boxes text-center pt-5" style="background: #7a67ee">
             <h6 class="mb-3">Service History</h6>
             <button class="btn btn-link border-light mb-5">
-              <a class="text-center text-light text-decoration-none" href="/servicelist">View History</a>
+              <a class="text-center text-light text-decoration-none" href="{{route('service.index')}}">View History</a>
             </button>
           </div>
         </div>
