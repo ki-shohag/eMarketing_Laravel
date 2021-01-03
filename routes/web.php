a7894548c6b7d68bef40279d5b1a452889df6f77
Al Zilqad <alzilqad@gmail.com>
2021-01-03 17:52:08 +0600

    Registration functionality added

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\clientUser\loginController;
 use App\Http\Controllers\clientUser\logoutController;
+use App\Http\Controllers\clientUser\registrationController;
 use App\Http\Controllers\clientUser\homeController;
 
 Route::get('/', function () {
@@ -13,5 +14,8 @@ Route::get('/', function () {
 Route::get('/login', [loginController::class, 'index'])->name('login.index');
 Route::post('/login', [loginController::class, 'verify']);
 Route::get('/logout', [logoutController::class, 'index'])->name('logout.index');
+
+Route::get('/registration', [registrationController::class, 'index'])->name('registration.index');
+Route::post('/registration', [registrationController::class, 'create']);
 
 Route::get('/home', [homeController::class, 'index'])->name('home.index');
